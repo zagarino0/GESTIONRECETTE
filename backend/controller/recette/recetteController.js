5e73df8ff5f02e78d07dceb784fff80f5c6ca243
Jus Winny Herivao <winny.herivao@spoonconsulting.com>
2024-01-03 00:17:21 +0300

    update

--- backend/controller/recette/recetteController.js
+++ backend/controller/recette/recetteController.js
@@ -181,24 +181,24 @@ const getRecapRecette = (req, res) => {
 
 const getClientByRecepisseAndDate = (req, res) => {
     const numero_recepisse = req.body.numero_recepisse;
-    const date = new Date(req.body.date)
+    const date = req.body.date
 
     let client = [];
 
-    if(numero_recepisse === ""){
+    if(numero_recepisse.length === 0){
         data.clients.map(cli => {
             data.charges.map(cha => {
                 data.modePayment.map(mod => {
-                    if(mod.numero_recepisse === numero_recepisse && cli.nif === cha.reference_fiscal && (new Date(mod.date_creation)) == date)
+                    if(mod.numero_recepisse == numero_recepisse && cli.nif === cha.reference_fiscal && date_creation == date)
                         client.push({...cli, ...cha, ...mod});
                 })
             })
         })
-    }else if(numero_recepisse !== ""){
+    }else if(numero_recepisse.length !== 0){
         data.clients.map(cli => {
             data.charges.map(cha => {
                 data.modePayment.map(mod => {
-                    if(mod.numero_recepisse === numero_recepisse && cli.nif === cha.reference_fiscal && mod.numero_recepisse === numero_recepisse && (new Date(mod.date_creation)) == date)
+                    if(mod.numero_recepisse === numero_recepisse && cli.nif === cha.reference_fiscal && mod.numero_recepisse === numero_recepisse && mod.date_creation == date)
                         client.push({...cli, ...cha, ...mod});
                 })
             })
