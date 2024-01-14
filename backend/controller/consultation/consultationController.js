a1d2c78463383627e797a973fb2023901af9a7b1
Jus Winny Herivao <winny.herivao@spoonconsulting.com>
2024-01-14 15:39:54 +0300

    update

--- backend/controller/consultation/consultationController.js
+++ backend/controller/consultation/consultationController.js
@@ -20,6 +20,9 @@ const getClientByNif = (req, res) => {
     const contribuable = data.client.find(con => con.reference_fiscal = reference_fiscal);
     if (!contribuable)
         return res.status(404).json({ 'message': 'Contribuable introuvable' });
+    const charge = data.charge.find(cha => cha.reference_fiscal === contribuable.reference_fiscal);
+    if(!charge)
+        return res.status(404).json({'message': 'Contribuable non prise en charge'});
     contribuable.actionnaire = data.actionnaires.length === 0 ? [] : data.actionnaires.filter(act => act.id_contribuable === contribuable.id);
     contribuable.dirigeant = data.dirigeants.length === 0 ? [] : data.dirigeants.filter(dir => dir.id_contribuable === contribuable.id);
     contribuable.activite = data.activites.length === 0 ? {} : data.activites.find(act => act.id_contribuable === contribuable.id);
@@ -37,6 +40,9 @@ const getClientByNumeroStatistique = (req, res) => {
     if (!activite)
         return res.status(404).json({ 'message': 'Contribuable introuvable' });
     const contribuable = data.client.find(con => con.id === activite.id_contribuable);
+    const charge = data.charge.find(cha => cha.reference_fiscal === contribuable.reference_fiscal);
+    if(!charge)
+        return res.status(404).json({'message': 'Contribuable non prise en charge'});
     contribuable.actionnaire = data.actionnaires.length === 0 ? [] : data.actionnaires.filter(act => act.id_contribuable === contribuable.id);
     contribuable.dirigeant = data.dirigeants.length === 0 ? [] : data.dirigeants.filter(dir => dir.id_contribuable === contribuable.id);
     contribuable.activite = activite;
@@ -54,34 +60,27 @@ const getClientByAddresse = (req, res) => {
     if (!siege)
         return res.status(404).json({ 'message': 'Contribuable Introuvable' })
     const contribuable = data.client.filter(con => con.id === siege.id_contribuable);
+    const contribuables = [];
     contribuable.map(contrib => {
-        if (contrib.id === adresse.id_contribuable) {
-            contrib.actionnaire = data.actionnaires.length === 0 ? [] : data.actionnaires.filter(act => act.id_contribuable === contrib.id);
-            contrib.dirigeant = data.dirigeants.length === 0 ? [] : data.dirigeants.filter(dir => dir.id_contribuable === contrib.id);
-            contrib.activite = activite;
-            contrib.etablissement = data.activites.length === 0 ? [] : data.etablissements.filter(eta => eta.id_contribuable === contrib.id);
-            contrib.coordonnees = data.coordonnees.length === 0 ? {} : data.coordonnees.find(coo => coo.id_contribuable === contrib.id);
-            contrib.siege = data.sieges.length === 0 ? {} : data.sieges.find(sie => sie.id_contribuable === contrib.id);
-            contrib.autre = data.autres.length === 0 ? {} : data.autres.find(aut => aut.id_contribuable === contrib.id);
-            contrib.interlocuteur = data.autres.length === 0 ? {} : data.interlocuteurs.find(inter => inter.id_contribuable === contrib.id);
-        }
-    })
-    res.json(contribuable);
-}
-
-const getClientByNomCommercial = (req, res) => {
-    const nom_commercial = req.body.nom_commercial;
-    let clients = {};
-
-    data.client.map(cli => {
         data.charge.map(cha => {
-            if (cli.nom_commerciale === nom_commercial && cli.nif === cha.reference_fiscal) {
-                clients = { ...cli, ...cha };
+            if (contrib.id === adresse.id_contribuable && contrib.reference_fiscal === cha.reference_fiscal) {
+                contrib.actionnaire = data.actionnaires.length === 0 ? [] : data.actionnaires.filter(act => act.id_contribuable === contrib.id);
+                contrib.dirigeant = data.dirigeants.length === 0 ? [] : data.dirigeants.filter(dir => dir.id_contribuable === contrib.id);
+                contrib.activite = activite;
+                contrib.etablissement = data.activites.length === 0 ? [] : data.etablissements.filter(eta => eta.id_contribuable === contrib.id);
+                contrib.coordonnees = data.coordonnees.length === 0 ? {} : data.coordonnees.find(coo => coo.id_contribuable === contrib.id);
+                contrib.siege = data.sieges.length === 0 ? {} : data.sieges.find(sie => sie.id_contribuable === contrib.id);
+                contrib.autre = data.autres.length === 0 ? {} : data.autres.find(aut => aut.id_contribuable === contrib.id);
+                contrib.interlocuteur = data.autres.length === 0 ? {} : data.interlocuteurs.find(inter => inter.id_contribuable === contrib.id);
+                contribuables.push(contrib);
             }
         })
     })
-    res.json(clients);
-    client = {};
+    res.json(contribuables);
+}
+
+const getClientByNomCommercial = (req, res) => {
+    const nom_commercial = 
 }
 
 const getClientByCIN = (req, res) => {
